bcfe5f5a91fe1c1e148c2b30d1f58523ea7081bb
FedericoGre <federico.garea@gmail.com>
2024-04-27 21:21:32 -0300

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,43 @@ function ReloadButton() {
 };
 
 
+const action = useNavigationType();
+const location = useLocation();
+const pathname = location.pathname;
+
+useEffect(() => {
+  if (action !== "POP") {
+    window.scrollTo(0, 0);
+  }
+}, [action, pathname]);
+
+useEffect(() => {
+  let title = "";
+  let metaDescription = "";
+
+  switch (pathname) {
+    case "/":
+      title = "";
+      metaDescription = "";
+      break;
+  }
+
+  if (title) {
+    document.title = title;
+  }
+
+  if (metaDescription) {
+    const metaDescriptionTag = document.querySelector(
+      'head > meta[name="description"]'
+    );
+    if (metaDescriptionTag) {
+      metaDescriptionTag.content = metaDescription;
+    }
+  }
+}, [pathname]);
+
+
+
 const App = () => {
 
   return (
